eafe481141e9af2aa8d6e298dbfc8a354e934777
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-08-20 14:48:42 -0500

    Added experience list to loop through

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent implements OnInit {
-
+  phrase = ['DevOps', 'SDLC', 'TDD', 'Agile', 'Waterfall'];
+  name: string;
   constructor() { }
 
   ngOnInit() {
+    this.timeOut();
   }
 
+  timeOut() {
+    let count = 0;
+    setInterval(() => {
+      if (count === this.phrase.length) {
+        count = 0;
+      }
+      this.name = this.phrase[count];
+      count++;
+    }, 2000);
+
+  }
 }
