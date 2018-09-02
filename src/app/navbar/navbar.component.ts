eac02015097439bf4182ce2924aa5fad80607039
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-09-02 15:18:27 -0500

    Fixed navbar issues

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,6 +11,7 @@ declare var $: any;
 export class NavbarComponent {
 
 
+
   constructor() { }
 
   ngOnInit() {
@@ -18,26 +19,31 @@ export class NavbarComponent {
     this.navBar();
   }
 
-  toggleTitle() {
-    $('.title').fadeOut("slow");
+  toggleSidebar() {
+    $('.sidebar').fadeOut("slow");
+
   }
 
   toggle() {
     $('#menu').click(function () {
-      $('.title').fadeToggle();
-      $('.navbar').css('background-color', 'black');
+      $('.sidebar').fadeToggle();
     });
   }
 
 
   navBar() {
+
     var scroll_pos = 0;
     $(document).scroll(function () {
       scroll_pos = $(this).scrollTop();
       if (scroll_pos > 600) {
         $(".navbar").css('background-color', 'black');
+        $(".sidebar").css('background-color', 'black');
+
       } else {
         $(".navbar").css('background-color', 'transparent');
+        $(".sidebar").css('background-color', 'transparent');
+
       }
     });
 
