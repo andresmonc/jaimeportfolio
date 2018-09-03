80efb06e2b3e81cf6ba92d50f34dfccb4829952a
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-09-03 13:52:21 -0500

    Minor changes for new production release

--- functions/index.js
+++ functions/index.js
@@ -13,22 +13,25 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
 
-// Node mailer
-const nodemailer = require('nodemailer');
-const gmailEmail = encodeURIComponent(functions.config().gmail.email);
-const gmailPassword = encodeURIComponent(functions.config().gmail.password);
-const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
 
-exports.contactMessage = functions.https.onRequest((request, response) => {
+// This requires blaze plan
+
+// // Node mailer
+// const nodemailer = require('nodemailer');
+// const gmailEmail = encodeURIComponent(functions.config().gmail.email);
+// const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+// const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+
+// exports.contactMessage = functions.https.onRequest((request, response) => {
  
-    const mailOptions = {
-        to: 'jaimeandresmonc@gmail.com',
-        subject: `Information Request from hihihhi`,
-        html: `test`
-    };
-    return mailTransport.sendMail(mailOptions).then(() => {
-        response.send("Success");
-        return console.log('Mail sent to: test@example.com')
+//     const mailOptions = {
+//         to: 'jaimeandresmonc@gmail.com',
+//         subject: `Information Request from hihihhi`,
+//         html: `test`
+//     };
+//     return mailTransport.sendMail(mailOptions).then(() => {
+//         response.send("Success");
+//         return console.log('Mail sent to: test@example.com')
 
-});
-  });
+// });
+//   });
