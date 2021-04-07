819fdff05024fb05656b958b3aef3e1fc752235f
andresmonc <jaimeamonc@gmail.com>
2021-04-06 19:07:15 -0500

    new technologies enhancement!!!

--- src/app/body/technologies/technologies.component.ts
+++ src/app/body/technologies/technologies.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild,ViewChildren } from '@angular/core';
 import { slideIn, fadeInOut } from '../../animations';
+import { DragScrollComponent } from 'ngx-drag-scroll';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-technologies',
@@ -9,11 +11,32 @@ import { slideIn, fadeInOut } from '../../animations';
     slideIn, fadeInOut
   ]
 })
-export class TechnologiesComponent implements OnInit {
+
+export class TechnologiesComponent {
+  @ViewChild('nav', {read: DragScrollComponent}) ds: DragScrollComponent;
 
   constructor() { }
+  public selectedIndex = 0;
+  public cookies = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 
-  ngOnInit() {
+  moveLeft() {
+    this.ds.moveLeft();
+  }
+
+  moveRight() {
+    this.ds.moveRight();
+
+
+  }
+
+  moveTo(index) {
+    this.ds.moveTo(index);
+  }
+
+  ngAfterViewInit() {
+    this.ds.indexChanged.subscribe(index => {
+      this.selectedIndex = index;
+    })
   }
 
 }
