9c2e85cb1d6b8c3c54f3b230fff89b7b7374f557
andresmonc <jaimeandresmonc@gmail.com>
2021-04-06 23:31:45 -0500

    reduced html

--- src/app/body/technologies/technologies.component.ts
+++ src/app/body/technologies/technologies.component.ts
@@ -17,8 +17,25 @@ export class TechnologiesComponent {
 
   constructor() { }
   public selectedIndex = 0;
-  public cookies = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
   public hideSwipe = false;
+
+  public  icons = [
+    ["devicon-java-plain-wordmark colored", "#4169e1"],
+    ["devicon-go-line", "#29adbe"],
+    ["devicon-nodejs-plain-wordmark colored", "#6B5B95"],
+    ["devicon-git-plain-wordmark", "#F1502F"],
+    ["devicon-express-original-wordmark colored", "#fdfd96"],
+    ["devicon-angularjs-plain", "red"],
+    ["devicon-postgresql-plain-wordmark colored", "rgb(59, 189, 92)"],
+    ["devicon-docker-plain-wordmark colored", "black"],
+    ["devicon-css3-plain-wordmark colored", "aliceblue"],
+    ["devicon-python-plain-wordmark colored", "#4B8BBE"],
+    ["devicon-mongodb-plain-wordmark", "#4DB33D"],
+    ["devicon-scala-plain-wordmark colored", "#6B5B95"],
+    ["devicon-typescript-plain colored", "#aliceblue"],
+    ["devicon-javascript-plain colored", "black"],
+    ["devicon-amazonwebservices-plain-wordmark colored", "#6B5B95"],
+  ]
 
   hideSwipeToggle(){
     this.hideSwipe = true;
